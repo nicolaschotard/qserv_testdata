ecd98e6d7aba5fdc1fa490442cfaa83d59f65c0e
Nicoals Chotard <nchotard@in2p3.fr>
2017-02-01 16:00:04 +0100

    Change data and query

--- datasets/case06/queries/0004_test.sql
+++ datasets/case06/queries/0004_test.sql
@@ -1,3 +1,3 @@
-SELECT d.deepCoadd_forcedId, d.coord_ra_deg, d.coord_dec_deg, d.modelfit_CModel_mag
+SELECT d.deepCoadd_forced_srcId, d.coord_ra_deg, d.coord_dec_deg, d.modelfit_CModel_mag
 FROM deepCoadd_forced_src AS d
-WHERE d.deepCoadd_forcedId = 1100182716504
+WHERE d.deepCoadd_forced_srcId = 1100182716504
